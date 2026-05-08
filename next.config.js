5916df7e6dd219280ac9d42ca4cdb86c8c6cdfae
935544059-commits <935544059@qq.com>
2026-05-08 17:31:13 +0800

    v1.2

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export', // 关键：必须添加这一行来启用静态导出
+  images: {
+    unoptimized: true, // 静态导出通常需要禁用 Next.js 的图片优化功能
+  },
 }
 
 module.exports = nextConfig
