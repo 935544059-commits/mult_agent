a0443c854185599a19b4e92875c7aa36951d2562
935544059-commits <935544059@qq.com>
2026-05-09 14:18:42 +0800

    chore: enable static export for cloudflare pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
+  images: {
+    unoptimized: true,   // 静态导出必须关闭图片优化
+  },
 }
 
 module.exports = nextConfig
